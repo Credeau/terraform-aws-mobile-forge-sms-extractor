475b36b7c25890d8f404bd98b11cfe475dbe1ac3
Harshit Sharma <brown.hash@outlook.com>
2025-07-03 13:19:14 +0530

    add s3 uri in output for vocab, classification and regex

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,18 @@ output "asg_name" {
 output "alb_access_log_bucket" {
   value = var.enable_alb_access_logs ? aws_s3_bucket.access_logs[0].bucket : null
 }
+
+# Vocabulary Config File URI
+output "vocab_config_file_uri" {
+  value = "s3://${var.vocab_s3_bucket}/${var.vocab_file_s3_path}"
+}
+
+# Classification Config File URI
+output "classification_config_file_uri" {
+  value = "s3://${var.classification_s3_bucket}/${var.classification_file_s3_path}"
+}
+
+# Regex Config File URI
+output "regex_config_file_uri" {
+  value = "s3://${var.regex_s3_bucket}/${var.regex_file_s3_path}"
+}
